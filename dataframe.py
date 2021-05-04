509169e756a209269dddbda2b3850eb80f3c309a
carina1754 <bulebed1754@gmail.com>
2021-05-04 17:09:23 +0900

    hgbj

--- dataframe.py
+++ dataframe.py
@@ -4,7 +4,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
 result = []
-days=4
+days=32
 num=0
 datanum=0
 coindata = pd.DataFrame(result)
@@ -14,21 +14,22 @@ for day in range(days):
     for hour in range(24):
         
             if day < 10:
-                date="2021-03-0"+str(day)
+                date="2021-02-0"+str(day)
             else:    
-                date="2021-03-"+str(day)
+                date="2021-02-"+str(day)
             
             if hour < 10:
                 time = "0"+str(hour)+":00:00"
             else:
                 time = str(hour)+":00:00"
 
-            url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-MOC&count=60&to='+date+"%20"+time
+            url = 'https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=CRIX.UPBIT.KRW-BTC&count=60&to='+date+"%20"+time
             req = requests.get(f'{url}')
             data = req.json()
             print(url)
             for i,candle in enumerate(data):
                 result.append({
+                'time' : data[i]["candleDateTimeKst"],
                 'open' : data[i]["openingPrice"],
                 'high' : data[i]["highPrice"],
                 'low' : data[i]["lowPrice"],
@@ -41,4 +42,4 @@ for day in range(days):
             coindata= coindata.append(pd.DataFrame(result))
             result=[]
 
-coindata.to_csv(f'data.csv')
+coindata.to_csv(f'data1.csv')
